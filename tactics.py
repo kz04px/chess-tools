8fbbc894d8e2723f31dfc8c415bf43f803ce5564
kz04px <6536046+kz04px@users.noreply.github.com>
2019-03-13 02:57:41 +0000

    Rewrite

--- tactics.py
+++ tactics.py
@@ -6,144 +6,157 @@ import queue
 import threading
 import os
 
+q = queue.Queue()
+lock = threading.Lock()
+correct = 0
+incorrect = 0
 
-class Manager:
-    def __init__(self):
-        self.q = queue.Queue()
-        self.lock = threading.Lock()
+def read_epd(path):
+    with open(path) as f:
+        for line in f:
+            try:
+                board, results = chess.Board().from_epd(line)
 
-    def go(self, enginePath, movetime=100, numThreads=1, verbose=False):
-        if self.q.qsize() < 1:
-            print("ERROR: no positions loaded ({})".format(self.path))
-            return 0, 0
+                if not board.is_valid():
+                    continue
 
-        self.total = 0
-        self.incorrect = 0
+                if board.is_game_over():
+                    continue
 
-        threads = []
-        for i in range(numThreads):
-            t = threading.Thread(target=self.worker, args=([enginePath, movetime, verbose]))
-            t.start()
-            threads.append(t)
+                # Get best move
+                if "bm" in results.keys():
+                    move = results["bm"][0]
+                else:
+                    continue
 
-        for t in threads:
-            t.join()
+                # Check move legality
+                if move not in board.legal_moves:
+                    continue
 
-        print("{}/{}  {:.2f}%  {}".format(self.total - self.incorrect, self.total, 100.0*(self.total - self.incorrect)/self.total, self.path))
-        return self.total - self.incorrect, self.total
+                q.put((board, move))
+            except:
+                pass
 
-    def load(self, path):
-        self.q.queue.clear()
-        self.path = path
-        try:
-            with open(path, "r") as f:
-                for line in f:
-                    try:
-                        asd = chess.Board().set_epd(line)
-                        if "bm" in asd:
-                            self.q.put(line)
-                    except:
-                        pass
-        except Exception as e:
-            print(e)
-            return False
-        return True
+def worker(path, movetime, verbose):
+    global correct
+    global incorrect
 
-    def worker(self, enginePath, timeper, verbose):
-        engine = chess.uci.popen_engine(enginePath)
-        engine.uci()
-        engine.isready()
+    try:
+        e = chess.uci.popen_engine(path)
+        e.uci()
+        e.isready()
 
-        board = chess.Board()
+        while not q.empty():
+            board, answer = q.get()
 
-        while self.q.empty() == False:
-            line = self.q.get()
+            e.ucinewgame()
+            e.position(board)
+            bestmove, _ = e.go(movetime=movetime)
 
-            asd = board.set_epd(line)
+            with lock:
+                if bestmove == answer:
+                    correct += 1
+                else:
+                    incorrect += 1
 
-            self.lock.acquire()
-            self.total += 1
-            self.lock.release()
+        e.quit()
+    except:
+        pass
 
-            engine.position(board)
+def run(path, num_threads, movetime, verbose):
+    global correct
+    global incorrect
 
-            command = engine.go(movetime=timeper, async_callback=True)
-            bestmove, ponder = command.result()
+    correct = 0
+    incorrect = 0
 
-            if bestmove != asd["bm"][0]:
-                self.lock.acquire()
-                if verbose:
-                    print("Got {}  expected {}  position {}".format(bestmove, asd["bm"][0], board.fen()))
-                self.incorrect += 1
-                self.lock.release()
+    # Create worker threads
+    threads = []
+    for i in range(num_threads):
+        t = threading.Thread(target=worker, args=(path, movetime, verbose))
+        threads.append(t)
 
-        engine.quit()
+    # Start working
+    for t in threads:
+        t.start()
 
+    # Wait
+    for t in threads:
+        t.join()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='UCI chess engine tactics')
-    parser.add_argument("-engine", type=str, help="path to the engine")
-    parser.add_argument("-suite", type=str, help="path to the test suite")
-    parser.add_argument("-movetime", type=int, help="search movetime")
-    parser.add_argument("-threads", type=int, help="threads to use")
+    return correct, incorrect
+
+def main():
+    parser = argparse.ArgumentParser(description='UCI chess engine perft')
+    parser.add_argument("-engine", type=str, required=True, help="path to the engine")
+    parser.add_argument("-suite", type=str, required=True, help="path to the test suite")
+    parser.add_argument("-movetime", type=int, default=100, help="search movetime")
+    parser.add_argument("-threads", type=int, default=1, help="threads to use")
     parser.add_argument("-verbose", help="print extra details", action='store_true')
     args = parser.parse_args()
 
-    if args.movetime < 1:
-        print("ERROR: movetime must be >= 1")
-        exit(1)
+    if not os.path.isfile(args.engine):
+        print("Engine file not found")
+        return
 
-    if args.movetime > 10000:
-        print("WARNING: that's a lot of movetime")
-
-    if args.threads < 1:
-        print("ERROR: threads must be >= 1")
-        exit(2)
-
-    if args.threads > 4:
-        print("WARNING: that's a lot of threads")
-
-    if os.path.isfile(args.engine) == False:
-        print("Cannot find engine specified")
-        exit(3)
-
+    # Load positions from .epd
     tests = []
-
     if os.path.isdir(args.suite):
         for root, dirs, files in os.walk(args.suite):
             for file in files:
                 if file.endswith(".epd"):
                     tests.append(os.path.join(root, file))
     elif os.path.isfile(args.suite):
-        tests.append(args.suite)
+        tests.append(os.path.join(root, file))
     else:
         print("No valid positions found at the path specified")
-        exit(4)
-
+        return
     tests = sorted(tests)
-    correct = 0
-    total = 0
 
-    print("Settings")
-    print("Engine: {}".format(args.engine))
-    print("Movetime: {}ms".format(args.movetime))
-    print("Threads: {}".format(args.threads))
+    args.movetime = max(args.movetime, 1)
+    args.threads  = max(args.threads, 1)
+
+    # Stats
+    total_correct = 0
+    total_incorrect = 0
+    total_time = 0
+
+    try:
+        for test in tests:
+            read_epd(test)
+
+            # Start timing
+            t0 = time.time()
+
+            correct, incorrect = run(args.engine, args.threads, args.movetime, args.verbose)
+
+            # Stop timing
+            t1 = time.time()
+
+            head, tail = os.path.split(test)
+
+            total = correct + incorrect
+            print(F"{correct}/{total}  {100*correct/total:.1f}%  {tail}")
+
+            total_correct += correct
+            total_incorrect += incorrect
+            total_time += t1 - t0
+    except:
+        pass
     print("")
 
-    start = time.time()
-    tactics = Manager()
-    print("Tests")
-    for test in tests:
-        tactics.load(test)
-        c, t = tactics.go(args.engine, args.movetime, args.threads, args.verbose)
-        correct = correct + c
-        total = total + t
-    end = time.time()
-    print("")
+    total_tests = total_correct+total_incorrect
+    if total_tests == 0:
+        print("No tests were completed")
+        return
 
-    if total > 0:
-        print("Totals")
-        print("Time: {:.2f}s".format(end - start))
-        print("Correct: {}".format(correct))
-        print("Total: {}".format(total))
-        print("Accuracy: {:.2f}%".format(100.0*correct/total))
+    # Results
+    print(F"Time:      {total_time:.2f}s")
+    print(F"Movetime:  {args.movetime}ms")
+    print(F"Threads:   {args.threads}")
+    print(F"Correct:   {total_correct} ({100*total_correct/total_tests:.1f}%)")
+    print(F"Incorrect: {total_incorrect} ({100*total_incorrect/total_tests:.1f}%)")
+    print(F"Total:     {total_tests}")
+
+if __name__ == "__main__":
+    main()
